cc4a9b8460786a9789bc9c9c47f52754016366a3
jneo8 <jneo8lin@gmail.com>
2020-03-02 11:54:20 +0800

    logger.go

---
+++ logger.go
@@ -0,0 +1,17 @@
+package mermaid
+
+import (
+	"github.com/onrik/logrus/filename"
+	log "github.com/sirupsen/logrus"
+)
+
+// NewLogger return default logger.
+func NewLogger() *log.Logger {
+	logger := log.New()
+	logger.AddHook(filename.NewHook())
+	logger.SetFormatter(&log.TextFormatter{
+		TimestampFormat: "2006-01-02T15:04:05.000",
+		FullTimestamp:   true,
+	})
+	return logger
+}
