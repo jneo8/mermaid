e320693e64f732188b3ab2fc1ad832f65387b10c
jneo8 <jneo8lin@gmail.com>
2020-03-06 15:14:03 +0800

    Add example.go

---
+++ cmd/example.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jneo8/mermaid"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.uber.org/dig"
+	"os"
+)
+
+type Service interface {
+	Run() error
+}
+
+type HttpService struct {
+	Engine   *gin.Engine
+	Username string
+}
+
+func (s *HttpService) Run() error {
+	s.Engine.GET("ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	s.Engine.GET("user", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": s.Username})
+	})
+	return s.Engine.Run()
+}
+
+type ServiceOptions struct {
+	dig.In
+	Username string `name:"username"`
+}
+
+func NewHttpService(opts ServiceOptions) Service {
+	log.Infof("New HpptService %v", opts)
+	return &HttpService{
+		Engine:   gin.Default(),
+		Username: opts.Username,
+	}
+}
+
+var cmd = &cobra.Command{
+	Use:   "exmaple",
+	Short: "exmaple",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		initializers := []interface{}{
+			NewHttpService,
+		}
+		runable := func(service Service, logger *log.Logger, config *viper.Viper) error {
+			logger.Infof("Config: %v", config.AllSettings())
+			if err := service.Run(); err != nil {
+				logger.Error(err)
+			}
+			return nil
+		}
+		return mermaid.Run(cmd, runable, initializers...)
+	},
+}
+
+func init() {
+	cmd.Flags().String("username", "userA", "username")
+}
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/jneo8/mermaid
 go 1.14
 
 require (
+	github.com/gin-gonic/gin v1.5.0
 	github.com/onrik/logrus v0.5.1
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.2
+	github.com/tidwall/gjson v1.6.0 // indirect
 	go.uber.org/dig v1.8.0
 )
