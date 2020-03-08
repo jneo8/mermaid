ad7b822affcbe0684f38cd518070f66b2bcbef07
jneo8 <jneo8lin@gmail.com>
2020-03-08 22:12:58 +0800

    Update cmd/example_test.go

--- cmd/example_test.go
+++ cmd/example_test.go
@@ -4,17 +4,12 @@ import (
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"net/http"
-	"sync"
 	"testing"
 	"time"
 )
 
 func TestRootCMD(t *testing.T) {
-	wg := sync.WaitGroup{}
-
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
 		// Overwrite config using args.
 		rootCMD.SetArgs(
 			[]string{"--username", "userB"},
